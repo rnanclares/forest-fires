0bfc1abe6ca2ed02f6d1a02f083c4e95f4c625c4
alequech <alequwch@gmail.com>
2021-02-08 17:06:43 -0600

    datos para el modelo

--- 3_extracion.R
+++ 3_extracion.R
@@ -11,21 +11,16 @@
 library(lubridate)
 library(raster)
 library(stars)
+library(terra)
 library(rgee)
 library(sf)
 library(tidyverse)
 library(magrittr)
+source("funciones.R")
 
 Sys.setenv(EARTHENGINE_PYTHON='/home/alequech/anaconda3/envs/gee/bin')
 Sys.setenv(RETICULATE_PYTHON='/home/alequech/anaconda3/envs/gee/bin')
 
-
-muestras <- "muestra/poligonos_muestra.gpkg"
-nlayers <- st_layers(muestras)[10]
-
-roi <- st_read(muestras, layer ="2010-02-01") %>%  sf_as_ee()
-
-as.Date(nlayers$name)  %m+% months(1) 
 #setwd("~/Documents/iA_LULC")
 #/home/alequech/.virtualenvs/rgee/bin/python 
 # ee_install()
@@ -35,61 +30,97 @@ ee_check()
 
 
 # Initialize Earth Engine!
-##ee_Initialize("alexander.jalisco")
-
-fecha_ini <- as.Date("2010-01-01")
-
-fecha_init_m <- fecha_ini # %>% as.character()
-
-fecha_fin_m <- (fecha_init_m %m+% months(1) - 1)  %>% as.character()
-
-fecha_init_m %<>% as.character()
-
-### NDVI modis NDVI, Evapotranspiracion y tempratura 
-
-modis_ndvi <- ee$ImageCollection("MODIS/006/MOD13Q1")
-### NDVI
-ndvi_composite <- modis_ndvi$
-  filter(ee$Filter$date(fecha_init_m,fecha_fin_m))$
-  filter(ee$Filter$calendarRange(1, field = "month"))$
-  map(mod1q1_clean)$
-  mean()
-
-##Evapotranspiracion 
-modis_evapo <- ee$ImageCollection("MODIS/NTSG/MOD16A2/105")$
-  select(c("PET","ET"))$
-  filter(ee$Filter$date(fecha_init_m,fecha_fin_m))$
-  filter(ee$Filter$calendarRange(1, field = "month"))$
-  mean()
-
-## Temperatura  
-modis_temp <- ee$ImageCollection("MODIS/006/MOD11A1")$
-  select("LST_Day_1km")$
-  filter(ee$Filter$date(fecha_init_m,fecha_fin_m))$
-  filter(ee$Filter$calendarRange(1, field = "month"))$
-  mean()
-
-### SMOS
-smos <- ee$ImageCollection("NASA_USDA/HSL/soil_moisture")$
-        filter(ee$Filter$date('2010-01-01', '2019-12-31'))$
-        select(c("ssm","susm"))$
-        mean()  
+ee_Initialize("alexander.jalisco")
 
 
-## Precipitacion
+periodo <- seq(as.Date("2010/02/01"), as.Date("2019/12/01"), "month")  
 
 
-ee_utils_create_json
+###  Terreno 
 
-datos <- ee_utils_create_json(list(ndvi_composite,smos))
-datos <- datos$addBands(smos)
+DEM <- ee$Image("USGS/SRTMGL1_003")
+Slope_T <- ee$Terrain$slope(DEM)
 
 
-ee_nc_rain <- ee_extract(
-  x = smos,
-  y = roi,
-  scale = 250,
-  fun = ee$Reducer$mean(),
-  sf = TRUE
-)
+n_list <- list()
+for(i in 1:length(periodo)){
+  
+  nlayer <-  periodo[i] %>% as.character()
+  
+  roi <-
+    st_read("muestra/poligonos_muestra.gpkg", layer = nlayer) %>%  sf_as_ee()
+  
+  fecha_init_m <- periodo[i] %m-% months(1) 
+  
+  fecha_fin_m <- (periodo[i] - 1)  %>% as.character()
+  
+  fecha_init_m %<>% as.character()
+  
+  print(paste0("mes incendio: ",nlayer))
+  print(paste0("muestreo: ", fecha_init_m," ", fecha_fin_m))
+  
+  
+  ### NDVI modis NDVI, Evapotranspiracion y tempratura 
+  
+  modis_ndvi <- ee$ImageCollection("MODIS/006/MOD13Q1")
+  ### NDVI
+  ndvi_composite <- modis_ndvi$
+    filter(ee$Filter$date(fecha_init_m,fecha_fin_m))$
+    map(mod13q1_clean)$
+    mean()
+  
+  ##Evapotranspiracion 
+  modis_evapo <- ee$ImageCollection("MODIS/006/MOD16A2")$
+    select(c("PET","ET"))$
+    filter(ee$Filter$date(fecha_init_m,fecha_fin_m))$
+    mean()
+  
+  ## Temperatura  
+  modis_temp <- ee$ImageCollection("MODIS/006/MOD11A1")$
+    select("LST_Day_1km")$
+    filter(ee$Filter$date(fecha_init_m,fecha_fin_m))$
+    mean()
+  
+  
+  ### SMOS
+  smos <- ee$ImageCollection("NASA_USDA/HSL/soil_moisture")$
+    filter(ee$Filter$date(fecha_init_m,fecha_fin_m))$
+    select(c("ssm","susm"))$
+    mean()  
+  
+  
+  ###  Precipitacion
+  precipitacion <- ee$ImageCollection("UCSB-CHG/CHIRPS/PENTAD")$
+    filter(ee$Filter$date(fecha_init_m,fecha_fin_m))$
+    sum()
+  
+
+  #####  stack GEE 
+  
+  stack_gee <- ndvi_composite$addBands(modis_evapo)
+  stack_gee <- stack_gee$addBands(modis_temp)
+  stack_gee <- stack_gee$addBands(smos)
+  stack_gee <- stack_gee$addBands(precipitacion)
+  stack_gee <- stack_gee$addBands(DEM)
+  stack_gee <- stack_gee$addBands(Slope_T)
+  
+  
+  datos <- ee_extract(
+    x = stack_gee,
+    y = roi,
+    scale = 250,
+    fun = ee$Reducer$mean(),
+    sf = TRUE
+  )
+  
+  rm(stack_gee)
+  
+  n_list[[nlayer]]  <- datos
+  
+}
+
+sapply(names(n_list),
+       function (x)
+         st_write(n_list[[x]], "muestra/poligonos_muestra_datos.gpkg", x, append = TRUE))
+
 
